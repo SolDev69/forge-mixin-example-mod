8e32c0f9151d375e62ed63f1423731b7795a1735
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-08 10:12:05 -0400

    Added forge 1.16.1

---
+++ src/main/java/com/example/examplemod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.example.examplemod.mixin;
+
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import org.apache.logging.log4j.LogManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MainMenuScreen.class)
+public class ExampleMixin {
+    @Inject(method = "<init>()V", at = @At("HEAD"))
+    private static void exampleMixin(CallbackInfo ci){
+        LogManager.getLogger().info("Hello from an example mixin!");
+    }
+}
